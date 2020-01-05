743e8a00a288b9b0ca02202e84df77a30d8e7e30
ZaoLahma <thyberg.jan@gmail.com>
2020-01-05 23:25:24 +0100

    Fix different integer width comparisons

--- serial_comm/TaskBase.cpp
+++ serial_comm/TaskBase.cpp
@@ -1,14 +1,13 @@
 #include "TaskBase.h"
 
 TaskBase::TaskBase(const uint16_t _runPeriodicity) : 
-runPeriodicity(_runPeriodicity), currTimeStamp(UINT16T_MAX)
+runPeriodicity(_runPeriodicity), currTimeStamp(0)
 {
 }
 
 void TaskBase::run(const unsigned long newTimeStamp)
 {
-  if ((UINT16T_MAX == currTimeStamp) ||
-      ((newTimeStamp - currTimeStamp) >= runPeriodicity))
+  if ((newTimeStamp - currTimeStamp) >= runPeriodicity)
   {
     currTimeStamp = newTimeStamp;
 
--- serial_comm/TaskBase.h
+++ serial_comm/TaskBase.h
@@ -8,14 +8,13 @@
 class TaskBase
 {
   public:
-  static const uint16_t UINT16T_MAX = 0xFFFFu;
   void run(const unsigned long newTimeStamp);
 
   TaskBase(const uint16_t _runPeriodicity);
 
   protected:
   const uint16_t runPeriodicity;
-  uint16_t currTimeStamp = 0u;
+  long currTimeStamp = 0u;
     
   virtual void execute(const unsigned long newTime) = 0;
 
